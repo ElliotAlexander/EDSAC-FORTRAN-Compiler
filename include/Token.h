0ef0a009b3b7a6dc259b38c01aac8d7ef3e30bf5
Elliot Alexander <ep1e16@soton.ac.uk>
2019-03-12 09:47:21 +0000

    [Feat] Updating Token to reflect changes to Token classes.

--- include/Token.h
+++ include/Token.h
@@ -6,8 +6,10 @@
 #include "Utils.h"
 #include "Constants.h"
 #include "RDParseTreeNode.h"
+#include "ThreeOpCode/ThreeOpCode.h"
 #include <boost/regex.hpp>
 #include <boost/algorithm/string.hpp>
+#include <memory>
 
 class Token{
     public:
@@ -15,7 +17,7 @@ class Token{
         virtual std::string getRegex() = 0;
         virtual std::string getTokenName() = 0;
         virtual bool initaliseToken(std::string input) = 0;
-        virtual std::vector<std::string> generatetoc(int &variable_index) = 0;
+        virtual std::vector<std::shared_ptr<ThreeOpCode> > generatetoc() = 0;
     private:
 };
 
