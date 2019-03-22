9e85732dfa240fa03932af415557c7de6a93c835
Elliot Alexander <ep1e16@soton.ac.uk>
2019-03-22 05:03:59 +0000

    [Feat] implementing NoRepeatedAcc optimisations.

--- src/Optimisation/NoRepeatedAccClear.cpp
+++ src/Optimisation/NoRepeatedAccClear.cpp
@@ -1,1 +1,15 @@
 #include "Optimization/NoRepeatedAccClear.h"
+
+NoRepeatedAccClear::NoRepeatedAccClear() {
+}
+
+std::vector<std::shared_ptr<ThreeOpCode> > NoRepeatedAccClear::processProgram(std::vector<std::shared_ptr<ThreeOpCode> > input) {
+    for(int index = 0; index < input.size() - 1; index++){
+        if(input.at(index)->getOperation() == THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR &&
+            input.at(index)->getOperation() == THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR){
+                std::shared_ptr<ThreeOpCode> ptr;
+                input.erase(std::remove(input.begin(), input.end(), ptr), input.end());
+        }
+    }
+    return input;
+}
