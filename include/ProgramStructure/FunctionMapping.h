f72adb99d5a8ad57770b6c8ddd4737a401479dee
Elliot Alexander <ep1e16@soton.ac.uk>
2019-03-24 19:58:41 +0000

    [Refactor] Modifying function mappings

--- include/ProgramStructure/FunctionMapping.h
+++ include/ProgramStructure/FunctionMapping.h
@@ -12,23 +12,23 @@
 #include "Globals.h"
 #include "ThreeOpCode/ThreeOpCode.h"
 
-struct FUNCTION_MAPPING_ENTRY {
+struct ARITH_FUNCTION_MAPPING_ENTRY {
     // The return value for the function exists at this address.
     std::shared_ptr<ST_ENTRY> value_return_address;
     std::vector<std::shared_ptr<ThreeOpCode> > function_body;
     std::vector<std::shared_ptr<ST_ENTRY> > arguments;
 };
 
-struct FUNCTION_MAPPING_RETURN {
+struct ARITH_FUNCTION_MAPPING_RETURN {
     bool found;
     std::vector<std::shared_ptr<ThreeOpCode> > function_body;
     std::shared_ptr<ST_ENTRY> return_val;
 };
 
-static std::map<std::string, FUNCTION_MAPPING_ENTRY> arithmetic_function_mappings;
+extern std::map<std::string, ARITH_FUNCTION_MAPPING_ENTRY> arithmetic_function_mappings;
 
-FUNCTION_MAPPING_ENTRY addArithmeticFunctionMapping(std::string function_name, std::vector<std::shared_ptr<ST_ENTRY> > arguments, std::vector<std::shared_ptr<ThreeOpCode> > function_body, std::shared_ptr<ST_ENTRY> return_addr);
-FUNCTION_MAPPING_RETURN getArithmeticFunctionMapping(std::string function_name, std::vector<std::shared_ptr<ST_ENTRY> > arguments);
+ARITH_FUNCTION_MAPPING_ENTRY addArithmeticFunctionMapping(std::string function_name, std::vector<std::shared_ptr<ST_ENTRY> > arguments, std::vector<std::shared_ptr<ThreeOpCode> > function_body, std::shared_ptr<ST_ENTRY> return_addr);
+ARITH_FUNCTION_MAPPING_RETURN getArithmeticFunctionMapping(std::string function_name, std::vector<std::shared_ptr<ST_ENTRY> > arguments);
 
 
 
