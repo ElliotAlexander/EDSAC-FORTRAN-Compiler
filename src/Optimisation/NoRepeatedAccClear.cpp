de01adcab8d5721f8e2600af1109911e6ac7c60f
Elliot Alexander <ep1e16@soton.ac.uk>
2019-03-25 17:45:48 +0000

    [Fix] Fixing an entirely broken Algorithm for removing repeated AccClears.

--- src/Optimisation/NoRepeatedAccClear.cpp
+++ src/Optimisation/NoRepeatedAccClear.cpp
@@ -6,9 +6,10 @@ NoRepeatedAccClear::NoRepeatedAccClear() {
 std::vector<std::shared_ptr<ThreeOpCode> > NoRepeatedAccClear::processProgram(std::vector<std::shared_ptr<ThreeOpCode> > input) {
     for(int index = 0; index < input.size() - 1; index++){
         if(input.at(index)->getOperation() == THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR &&
-            input.at(index)->getOperation() == THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR){
-                std::shared_ptr<ThreeOpCode> ptr;
-                input.erase(std::remove(input.begin(), input.end(), ptr), input.end());
+            input.at(index + 1)->getOperation() == THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR) {
+                for (int erase_index = index; erase_index < input.size() - 1; ++erase_index) {
+                    input[erase_index] = input[erase_index + 1]; 
+                }
         }
     }
     return input;
