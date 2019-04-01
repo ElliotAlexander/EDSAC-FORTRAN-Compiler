216ac07641daddef9637805517a764d208c9a93b
Elliot Alexander <epurvis97@gmail.com>
2019-04-01 09:20:48 +0100

    [Feat] Adding pass-by-reference mapping for ints in function calls

--- src/ArithmeticParser/Function.cpp
+++ src/ArithmeticParser/Function.cpp
@@ -1,24 +1,41 @@
 #include "ArithmeticParser/Function.h"
 
-Function::Function(std::vector<RDParseTreeNode *> args_in, std::string function_name_in) : function_arguments(args_in), function_name(function_name_in)
+Function::Function(std::vector<RDParseTreeNode *> args_in, std::string function_name_in) : 
+    function_arguments(args_in), 
+    function_name(function_name_in)
 {
     // TODO implement arguments
     Logging::logInfoMessage(function_name);
     tt = TOC_TYPES::FUNCTION_E;
 }
 
-TOC_RETURN_VALUE Function::generateThreeOPCode(){
+TOC_RETURN_VALUE Function::generateThreeOPCode(int &starting_address){
     std::vector<std::shared_ptr<ThreeOpCode> > return_string;
     std::vector<std::shared_ptr<ST_ENTRY> > arguments;
     for(std::vector<RDParseTreeNode*>::iterator it = function_arguments.begin(); it != function_arguments.end(); ++it)
     { 
-        TOC_RETURN_VALUE toc_res = (*it)->generateThreeOPCode();
+        TOC_RETURN_VALUE toc_res = (*it)->generateThreeOPCode(starting_address);
         return_string.insert(return_string.end(), toc_res.pre_string.begin(), toc_res.pre_string.end());
         arguments.push_back(toc_res.call_value);
     }
 
 
     ARITH_FUNCTION_MAPPING_RETURN function_mapping = ::getArithmeticFunctionMapping(function_name, arguments);
-    return_string.insert(return_string.end(), function_mapping.function_body.begin(), function_mapping.function_body.end());
-    return {function_mapping.function_body, function_mapping.return_val};
+    if(function_mapping.found){
+        return_string.insert(return_string.end(), function_mapping.function_body.begin(), function_mapping.function_body.end());
+        starting_address += return_string.size();
+        return {return_string, function_mapping.return_val};
+    } else {
+
+        // TODO bring both funciton mappings in line with each other
+        FUNCTION_MAPPING_RETURN function_mapping = ::getFunctionMapping(function_name, arguments, starting_address);
+        if(function_mapping.result){
+            return_string.insert(return_string.end(), function_mapping.toc_inject.begin(), function_mapping.toc_inject.end());
+            starting_address += return_string.size();
+            return {return_string, function_mapping.return_val };
+        } else {
+            Logging::logErrorMessage("Function " + function_name + " not found!");
+            return {};
+        }
+    }
 }
