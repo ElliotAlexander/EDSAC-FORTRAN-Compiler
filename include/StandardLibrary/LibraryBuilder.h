da3a772940e3ce68df0a1bb5c3bd794aca8a8656
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-02 02:59:29 +0100

    [Fix] Broken line mappings, fancy pointer arithmetic

--- include/StandardLibrary/LibraryBuilder.h
+++ include/StandardLibrary/LibraryBuilder.h
@@ -23,6 +23,7 @@ namespace Libs {
 		std::map<std::string, Library*> createMap();
 		void enableRoutine(std::string name);
 		LibraryReturnContainer buildLibraries(int offset);
+		std::shared_ptr<int> getLibraryLineMapping(std::string name);
 }
 
 
--- src/StandardLibrary/P6.cpp
+++ src/StandardLibrary/P6.cpp
@@ -5,7 +5,7 @@ P6::P6(){
 	P6::subroutine_text  = "[P6]\nGKA3FT25@H29@VFT4DA3@TFH30@S6@T1FV4DU4DAFG26@TFTFO5FA4DF4FS4FL4FT4DA1FS3@G9@EFSFO31@E20@J995FJF!F..PZ";
 	P6::is_enabled = false;
 	P6::instruction_length = 56;
-	P6::calling_value = 0;
+	P6::calling_value = std::shared_ptr<int>(new int(0));
 	P6::name = "P6";
 	Logging::logInfoMessage("Initialised P6 library.");
 }
@@ -37,6 +37,8 @@ std::shared_ptr<int> P6::getCallingValue(){
 
 
 bool P6::applyOffset(int offset) {
-	P6::calling_value.reset(P6::calling_value.get() + offset);
+	int new_val = *calling_value + offset;
+	*calling_value = new_val;
+	Logging::logMessage("Offset P6 call value to " + std::to_string(*calling_value));
 	return true;
 }
