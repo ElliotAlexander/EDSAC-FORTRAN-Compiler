5e4b8ca8ef7166dcc61ea364049a09f4745b3ff5
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-02 11:17:20 +0100

    [Fix] Adding correct line mapping for PRINT instructions.

--- src/Tokens/PRINT.cpp
+++ src/Tokens/PRINT.cpp
@@ -12,13 +12,10 @@ bool PRINT::initaliseToken(std::string input){
 	}
 }	
 
-
-
 std::vector<std::shared_ptr<ThreeOpCode>> PRINT::generatetoc(int starting_address){
 	std::vector<std::shared_ptr<ThreeOpCode> > pre_string;
 
 	TOC_RETURN_VALUE toc_ret = PRINT::print_val->generateThreeOPCode(starting_address);
-	pre_string.insert(pre_string.begin(), toc_ret.pre_string.begin(), toc_ret.pre_string.end());
 
     ALL_ST_SEARCH_RESULT flush_to = SymbolTableController::getVariable(Globals::BUFFER_FLUSH_NAME);
     Logging::logConditionalErrorMessage(!flush_to.found, "Failed to find buffer flush ST_ENTRY!");
@@ -26,9 +23,11 @@ std::vector<std::shared_ptr<ThreeOpCode>> PRINT::generatetoc(int starting_addres
 
 	std::shared_ptr<int> P6_mapping = Libs::getLibraryLineMapping("P6");
 
+	std::shared_ptr<int> mapping = LineMapping::addTemporaryLineMapping(starting_address + 3);
+
 	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(toc_ret.call_value, THREE_OP_CODE_OPERATIONS::ADD_TO_ACCUMULATOR, false)));
 	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode("", THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR, false)));
-	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(std::to_string(starting_address + 2), THREE_OP_CODE_OPERATIONS::ADD_TO_ACCUMULATOR, false)));
+	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(mapping , THREE_OP_CODE_OPERATIONS::ADD_TO_ACCUMULATOR, false)));
 	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(P6_mapping, THREE_OP_CODE_OPERATIONS::ACCUMULATOR_IF_NEGATIVE, false)));
 	return pre_string;
 }
