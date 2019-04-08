a23970c4198ba81027f64aba3d9a963fd0efce53
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-08 18:31:47 +0100

    [Fix] Refactoring no repeated acc to always remove the second (and hence unimportant) clear.

--- src/Optimisation/NoRepeatedAccClear.cpp
+++ src/Optimisation/NoRepeatedAccClear.cpp
@@ -7,7 +7,7 @@ std::vector<std::shared_ptr<ThreeOpCode> > NoRepeatedAccClear::processProgram(st
     for(int index = 0; index < input.size() - 1; index++){
         if(input.at(index)->getOperation() == THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR &&
             input.at(index + 1)->getOperation() == THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR) {
-                for (int erase_index = index; erase_index < input.size() - 1; ++erase_index) {
+                for (int erase_index = index + 1; erase_index < input.size() - 1; ++erase_index) {
                     input[erase_index] = input[erase_index + 1]; 
                 }
         }
