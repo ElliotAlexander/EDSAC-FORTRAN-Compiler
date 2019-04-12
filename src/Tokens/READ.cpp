d00301f31ff2ae8ba15ac19332b8d8067bdce69f
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-12 01:37:18 +0100

    [Feat] Updating Read, issues with M20 holding READ back.

--- src/Tokens/READ.cpp
+++ src/Tokens/READ.cpp
@@ -14,9 +14,12 @@ std::vector<std::shared_ptr<ThreeOpCode>> READ::generatetoc(int starting_address
     std::vector<std::shared_ptr<ThreeOpCode> > pre_string;
     ALL_ST_SEARCH_RESULT entry = SymbolTableController::getVariable(READ::variable_name);
     Logging::logConditionalErrorMessage(!entry.found, "Error - failed to find variable " + READ::variable_name + ".");
-    if(!found){
-        entry = SymbolTableController::addDeclaredVariable(READ::variable_name, "", ST_ENTRY_TYPE::UNASSIGNED_T);
+    std::shared_ptr<ST_ENTRY> use_entry;
+    if(!entry.found){
+        use_entry = SymbolTableController::addDeclaredVariable(READ::variable_name, "", ST_ENTRY_TYPE::UNASSIGNED_T);
         Logging::logInfoMessage("Adding new variable " + READ::variable_name);
+    } else {
+        use_entry = entry.result;
     }
 
     std::shared_ptr<int> m20_mapping = Libs::getLibraryLineMapping("P6");
@@ -26,8 +29,11 @@ std::vector<std::shared_ptr<ThreeOpCode>> READ::generatetoc(int starting_address
     Logging::logConditionalErrorMessage(!flush_to.found, "Failed to find buffer flush ST_ENTRY!");
 
 	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(flush_to.result, THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR, false)));
-	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(entry.result, THREE_OP_CODE_OPERATIONS::ADD_TO_ACCUMULATOR, false)));
+	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(mapping, THREE_OP_CODE_OPERATIONS::ADD_TO_ACCUMULATOR, false)));
 	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode("", THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACCUMULATOR_NO_CLEAR, false)));
 	pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(m20_mapping , THREE_OP_CODE_OPERATIONS::ACCUMULATOR_IF_POSTITIVE, false)));
+    pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode("45", THREE_OP_CODE_OPERATIONS::ADD_TO_ACCUMULATOR, false)));
+    pre_string.push_back(std::shared_ptr<ThreeOpCode>(new ThreeOpCode(use_entry, THREE_OP_CODE_OPERATIONS::TRANSFER_FROM_ACUMULATOR, false)));
+
     return pre_string;
 }
