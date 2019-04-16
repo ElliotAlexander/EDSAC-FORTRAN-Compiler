8ce538c9d959eef767362c7f6764f19368cdd101
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-16 23:23:28 +0100

    [Docs] Adding documentation to tokeens.

--- src/Token.cpp
+++ src/Token.cpp
@@ -1,5 +1,14 @@
 #include "Token.h"
 
+/**
+ * 
+ *   Abstract Class Token
+ * 
+ *      This class represents the top level abstraction for a program statement (a Token)
+ *      All tokens must implement the following methods, and Token* is used to pass around instances of tokens.
+ *      The only common method is isValid, which takes a tokens REGEX value as an input and validates whether it matches the input string.
+ **/
+
 bool Token::isValid(std::string input, std::string regex){
     boost::regex expr{regex};
     return (boost::regex_match(input, expr));
