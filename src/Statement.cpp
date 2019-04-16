1607b603442bdda83ec94823992c0f9f1523ec6a
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-16 22:47:18 +0100

    [Docs] Adding documentation to Statements

--- src/Statement.cpp
+++ src/Statement.cpp
@@ -1,5 +1,17 @@
 #include "Statement.h"
 
+
+// Statement::Statement(std::string statement_body, std::string label, int line_no)
+//
+// @param std::string statement_body -> The complete statement, with whitespace, without a label or continuation bit.
+// @param std::string label -> the statement line label
+// @param int line_no -> The statement line_number, as it was in the input file. This is used mainly for error messages.
+//
+// Note that Statement contains the member variable statement_body_no_ws, which is the statemebnt bodoy minus the (irrelevant) whitespace.
+//
+// This class acts as the main point of identifying statements. 
+//
+//
 Statement::Statement(std::string statement_body, std::string label, int line_no){
     Statement::statement_body = statement_body;
     Statement::label = label;
@@ -7,9 +19,23 @@ Statement::Statement(std::string statement_body, std::string label, int line_no)
     Statement::statement_body_no_ws = ::stripWhitespaceString(statement_body);
 }
 
+
+// IDENTIFY_STATEMENT_RESULT_T Statement::identifyStatement()
+// 
+// @return IDENTIFY_STATEMENT_RESULT_T -> A boolean indicating whether the statement was identified, and a pointer to the token object for that Statement.
+//
+// This function takes the body of the statement, and validates it against a list of possible tokens.
+// Once the correct token is found, it is removed from the list, and the list cleared. A pointer to the correct token is then
+// returned. The calling class then becomes responsible for that token. 
+//
+// Each token is identified by comparing it's associated REGEX string with the statement body.
+// If the string is a match, then token object is initialised. 
+// 
+//
 IDENTIFY_STATEMENT_RESULT_T Statement::identifyStatement(){
-
-
+    bool found = false;
+    Token* result;
+    
     DO* dostmt = new DO();
     SUBROUTINE* substmt = new SUBROUTINE();
     END* endstmt = new END();
@@ -33,6 +59,11 @@ IDENTIFY_STATEMENT_RESULT_T Statement::identifyStatement(){
 
 
     // TODO - There msut be a better way to do this. 
+
+    // Build an array of all possible Tokens. 
+    // Note taht this array will be cleared from the heap before the function exits.
+    // Responsibility for the <single> correct token is passed ot the calling function.
+    // 
     std::vector<Token*> tokens{
         dostmt,
         format_stmt,
@@ -55,46 +86,45 @@ IDENTIFY_STATEMENT_RESULT_T Statement::identifyStatement(){
         print_stmt,
         read_stmt
     };
-    bool found = false;
 
 
-    Token* result;
-
-    if(Globals::output_regex_matching_strings ){
+    if(Globals::output_regex_matching_strings ){        // Enabled via command line.
         for(std::vector<std::string>::size_type i = 0; i != tokens.size(); i++){ 
             Logging::logInfoMessage("[REGEX] " + tokens[i]->getRegex());
         }
-        Globals::output_regex_matching_strings = false;
+        Globals::output_regex_matching_strings = false; // we only need to do this once. Disable the flag after.
     }
 
     // Iterate through token types.
     for(std::vector<std::string>::size_type i = 0; i != tokens.size(); i++){ 
-        // Check token is valud - parse token type. 
-        if(tokens[i]->isValid(statement_body_no_ws, (tokens[i]->getRegex()))){
+        if(tokens[i]->isValid(statement_body_no_ws, (tokens[i]->getRegex()))){  // If the token matches the statement body (without whitespace), we have identified the token.
+            // unless the token has already been found - this gives us a problem.
+            // Inform the user, and continue optimistically.
             if(found){
                 Logging::logErrorMessage("Error parsing line - line matches two possible tokens [" + std::to_string(Statement::line_no + 1) + "]{" + statement_body + "}");
                 Logging::logErrorMessage("Second matching token: " + tokens[i]->getTokenName());
                 continue; 
             } 
+
             Logging::logConditionalMessage(Globals::dump_tokens, "[" + std::to_string(Statement::line_no + 1) + "][" + tokens[i]->getTokenName() + "] { " + statement_body + " }");
 
-            found = true;
-            result = tokens[i];
-            tokens[i] = nullptr;
-            if(Globals::lazy_tokens){
-                break;
+            found = true;                   // Once we've seen a token, set this flag. If we see another token that matches, warn the user.
+            result = tokens[i];             // Set the result. Note that if two tokemns are found (are more), the last one will be used.
+            tokens[i] = nullptr;            // Dereference the pointer in the array.
+            if(Globals::lazy_tokens){       // If this is enabled, break once we've found a single valid token.
+                break;                      // 
             }
         }
     };
 
-    if(!found){
+    if(!found){         // If the loop has finished and a token has not been found, warn the user. 
         Logging::logErrorMessage("Cannot find valid token for line [" + std::to_string(Statement::line_no + 1) + "]{" + statement_body + "}");
-        return IDENTIFY_STATEMENT_RESULT_T{nullptr, false};
+        return IDENTIFY_STATEMENT_RESULT_T{nullptr, false};      // The result flag is important for informing the calling class not to touch the Token pointer.
     } else {
         for(int index = 0; index < tokens.size(); index++){
-            delete(tokens[index]);
-        }
-        return IDENTIFY_STATEMENT_RESULT_T{result, true};
+            delete(tokens[index]);                              // If we have found the correct token, it's already been removmed from the array.
+        }                                                       //  Hence, clear the array, free the heap.
+        return IDENTIFY_STATEMENT_RESULT_T{result, true};       // return
     }
 }
 
