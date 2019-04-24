47bb10b8412669c6b15b7dd89e3f51d4c0e4344e
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-24 15:47:54 +0100

    [Docs] Adding code documentation

--- src/ArithmeticParser/Value.cpp
+++ src/ArithmeticParser/Value.cpp
@@ -1,1 +1,11 @@
 #include "ArithmeticParser/Value.h"
+
+/**
+ * 
+ * 
+ * 
+ * Value is implemented entirely in the header - this class is a stub.
+ * 
+ * Value represents a CONSTANT numerical value in the parse tree. All leaves of the parse tree are either variables or values.
+ * A value is a constant hard coded value. 
+ * 
