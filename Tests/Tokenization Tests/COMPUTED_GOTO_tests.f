678b10ee6c0c19674914adbe47d294bc9184049e
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-27 14:02:47 +0100

    [Test] Adding tokenization test for COMPUTED GOTO

--- Tests/Tokenization Tests/COMPUTED_GOTO_tests.f
+++ Tests/Tokenization Tests/COMPUTED_GOTO_tests.f
@@ -1,10 +1,15 @@
-1     ASSIGN X TO 2
+C	This should parse
       GOTO (5,10,15), X
-      STOP
-10    PRINT 10
-      GOTO 55
-15    PRINT 15
-      GOTO 55
-5     PRINT 5
-      GOTO 55
-55    END
+C     This should parse
+      GOTO (5,10,15), LONGERVARIABLE
+C 	This should parse
+      GOTO (10, 5), X
+C	This should pare
+      GOTO (10), X
+C 	This should not parse - we need at least one argument.
+      GOTO (), X
+C 	This shouldn't parse, the comparison variable canot be arithmetic
+      GOTO (5,10,15), 5
+C     This shouldn't parse, we can't call functions as comparison variables
+      GOTO (5,10, 15), SQRT(X)
+      END
