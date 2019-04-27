c618ac84db6acf123e8c65f63000237c5096e3f6
Elliot Alexander <ep1e16@soton.ac.uk>
2019-04-27 14:02:26 +0100

    [Test] Adding tokeniation tests for ASSIGNED GOTO

--- Tests/Tokenization Tests/ASSIGNED_GOTO_tests.f
+++ Tests/Tokenization Tests/ASSIGNED_GOTO_tests.f
@@ -1,10 +1,20 @@
-1     ASSIGN X TO 10
+C     This should parse. This is basically the only acceptable form of this statement.
+C
       GOTO X, (5,10,15)
-      STOP
-10    PRINT 10
-      GOTO 55
-15    PRINT 15
-      GOTO 55
-5     PRINT 5
-      GOTO 55
-55    END
+C     This shouldn't parse - the syntax is wrong.
+      GOTO 5, (X,Y,Z)
+C     This shouldn't parse - the first value should be a variable
+      GOTO X  (5, 10, 15)
+C     This shouldn't parse - only three values are allowed in the brackets for Assigned GOTOs.
+      GOTO X (5,5,5,5,5,5,5,5,5,5)
+C     This shouldn't parse - only one set of brackets is expected.
+      GOTO X() (5,5,5)
+C     This should't parse,  values in the brackets are required
+      GOTO X ()
+C     This shouldn't parse, three values are required in the brackets
+      GOTO X (5,5)
+C     This shouldn't parse
+      GOTO 10 (10, 15, 20)
+C     This should't parse- variables aren't allowed in the brackets
+      GOTO X (X, 10, 15)
+      END
